09fb313b8caaefcee10302df0bbc08629990eba7
lemonlux <sololucia17@gmail.com>
2023-09-27 15:09:14 +0200

    iteracion5

--- JAVASCRIPT/JS Basic Algorithms/ej.js
+++ JAVASCRIPT/JS Basic Algorithms/ej.js
@@ -84,24 +84,24 @@ let i = c*= m
 
 console.log(i)
 
-//!---------- ITERACION 4 ---------
+//!-------------------- ITERACION 4 -----------------
 
-//1.1
+//--------------1.1
 const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 
 console.log(avengers[0])
 
-//1.2
+//--------------1.2
 
 avengers[0]= "IRONMAN"
 
 console.log(avengers)
 
-//1.3 ?????
+//---------------1.3
 
 console.log(avengers.length)
 
-//1.4
+//----------------1.4
 
 const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"]
 
@@ -110,7 +110,7 @@ rickAndMortyCharacters.push("Summer")
 
 console.log(rickAndMortyCharacters[4])
 
-//1.5
+//------------------1.5
 
 const rickAndMortyCharactersAgain = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"]
 
@@ -118,8 +118,45 @@ rickAndMortyCharactersAgain.pop()
 
 console.log(rickAndMortyCharactersAgain[0], rickAndMortyCharactersAgain[4])
 
-// 1.6
+//--------------------1.6
 
 rickAndMortyCharactersAgain.splice(1, 1)
 
 console.log(rickAndMortyCharactersAgain)
+
+//!------------------ ITERACION 5 -----------------------
+
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+// ejemplo
+// if(number1 === 10){
+//     console.log('number1 es estrictamente igual a 10')
+// }
+
+if ((number2/number1) == 2 ) {
+ console.log("number2 dividido entre number1 es igual a 2");
+} else console.log("number1 dividido entre number2 no es igual a 2")
+
+if (number1 !== number2) {
+  console.log("number1 es estrictamente distinto a number2");
+}
+
+number1 !== number2 ? console.log("son estrictamente distintos") : console.log ("no son estrictamente distintos")
+
+if (number3 != number1) {
+ console.log("number3 es distinto number1");
+}
+
+if ((number3*5) == number1) {
+    console.log("number3 por 5 es igual a number1");
+}
+
+if (((number3*5) == number1) && ((number1*2) == number2)) {
+ console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+}
+
+ if ((number2/2) == number1 || (number1/5) == number3) {
+   console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+  }
