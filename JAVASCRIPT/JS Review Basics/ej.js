644f50a39ba0bef2f6e65ef854b0917c774663a4
lemonlux <sololucia17@gmail.com>
2023-10-01 18:15:31 +0200

    iteraciones

--- JAVASCRIPT/JS Review Basics/ej.js
+++ JAVASCRIPT/JS Review Basics/ej.js
@@ -56,53 +56,26 @@ const users = [
     },
 ]
 
-const averageVolume = []
+let averageVolume = []
+let volumes = []
+let sum = 0
+let total = 0
+
 
 for (let user of users){
-    for(let info in user){
-    //    console.log(user[info])
-    const sounds = Object.values(user[info])
- //   console.log(sounds)
-    }
-  //  console.log(user)
-  // const info = Object.values(user.favoritesSounds)
-   // const concatInfo = info.concat(info)
-        
-//console.log(info)
-   }
+    let sounds = user.favoritesSounds
+    for (let type in sounds){
+        let values = sounds[type]
+        for (let volume in values){
+//        console.log(values[volume])
+        (typeof values[volume] == "number") && (sum += values[volume])
+        (typeof values[volume] == "number") && (total++)
 
+    }
+    console.log(sum, total)
+}
+}
 
 
    //---------------- ITERACION 3 -----------------
 
-
-   const users = [
-    {name: 'Manolo el del bombo',
-        favoritesSounds: {
-            waves: {format: 'mp3', volume: 50},
-            rain: {format: 'ogg', volume: 60},
-            firecamp: {format: 'mp3', volume: 80},
-        }
-    },
-    {name: 'Mortadelo',
-        favoritesSounds: {
-            waves: {format: 'mp3', volume: 30},
-            shower: {format: 'ogg', volume: 55},
-            train: {format: 'mp3', volume: 60},
-        }
-    },
-    {name: 'Super Lopez',
-        favoritesSounds: {
-            shower: {format: 'mp3', volume: 50},
-            train: {format: 'ogg', volume: 60},
-            firecamp: {format: 'mp3', volume: 80},
-        }
-    },
-    {name: 'El culebra',
-        favoritesSounds: {
-            waves: {format: 'mp3', volume: 67},
-            wind: {format: 'ogg', volume: 35},
-            firecamp: {format: 'mp3', volume: 60},
-        }
-    },
-]
