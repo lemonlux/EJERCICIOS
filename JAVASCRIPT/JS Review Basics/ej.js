62efecc79875da96145f13249d36138f8d26b118
lemonlux <sololucia17@gmail.com>
2023-10-01 17:31:20 +0200

    iteracion2

--- JAVASCRIPT/JS Review Basics/ej.js
+++ JAVASCRIPT/JS Review Basics/ej.js
@@ -21,11 +21,62 @@ for (category of categories){
 console.log(categoriesArray)
 
 
-//------------- ITERACION 2 -----------------
+//------------- ITERACION 2 -----------------          ?????????
 
 console.log("-------2--------")
 
 const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+
+const averageVolume = []
+
+for (let user of users){
+    for(let info in user){
+    //    console.log(user[info])
+    const sounds = Object.values(user[info])
+ //   console.log(sounds)
+    }
+  //  console.log(user)
+  // const info = Object.values(user.favoritesSounds)
+   // const concatInfo = info.concat(info)
+        
+//console.log(info)
+   }
+
+
+
+   //---------------- ITERACION 3 -----------------
+
+
+   const users = [
     {name: 'Manolo el del bombo',
         favoritesSounds: {
             waves: {format: 'mp3', volume: 50},
