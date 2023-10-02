8704ea9c8cbbcb91cab43499ef81a69e6deb8ff9
lemonlux <sololucia17@gmail.com>
2023-10-02 15:42:08 +0200

    commiteo

--- JAVASCRIPT/JS Review Basics/ej.js
+++ JAVASCRIPT/JS Review Basics/ej.js
@@ -63,7 +63,7 @@ let volumes = 0
 let media = 0
 
 for (const user of users){
-    const {favoritesSounds} = user
+    const {favoritesSounds} = user              // destructuring
     for(let sound in favoritesSounds){
      //   console.log(favoritesSounds[sound])
          value = favoritesSounds[sound].volume
@@ -78,61 +78,89 @@ for (const user of users){
  console.log(media)         // el console.log tiene que estar fuera del bucle o te va saliendo tantos bucles haya
 
 
-
-
-
-
+// let volumes = 0
 
 // for (user of users){
 //     let sounds = user.favoritesSounds
 //    for (sound in sounds){
 //     let data = sounds[sound]
 //     for (let value in data){
-//    //     console.log(data[value])
-//   //  (typeOf (data[value]) == "number") && acc +=
+//     //   console.log(data[value])
+//    (typeof (data[value]) == "number") && (volumes = (volumes + data[value]))
 //    }
 // } 
-//  //  console.log(acc)
 // }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let averageVolume = []
-// let volumes = []
-// let sum = 0
-// let total = 0
-
-
-// for (let user of users){
-//     let sounds = user.favoritesSounds
-//     for (let type in sounds){
-//         let values = sounds[type]
-//         for (let volume in values){
-// //        console.log(values[volume])
-//         (typeof values[volume] == "number") && (sum += values[volume])
-//         (typeof values[volume] == "number") && (total++)
-
-//     }
-//     console.log(sum, total)
-// }
-// }
-
+//  console.log(volumes)
 
    //---------------- ITERACION 3 -----------------
 
+   console.log("-------3--------")
+
+   const users3 = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+
+const favorites = []
+
+for (user3 of users3){
+    const{favoritesSounds} = user3
+    for (keys in favoritesSounds){
+        favorites.push(keys)  
+    } 
+}
+
+const funcionContar = () =>{
+    let contador = [] 
+    favorites.forEach(word=>{
+    contador[word] ? contador[word]++ : contador[word] = 1
+    }
+    )
+    return contador
+}
+
+console.log(funcionContar(favorites))  
 
 
    // --------------- ITERACION 4 ------------------
+
+
+   const ejemplo = ['Caracol', 'Mosquito', 'Salamandra', 'Ajolote']
+
+
+   function findArrayIndex(array, text) {
+    if(array.indexOf(text)>=0){
+        return `si, ${array.indexOf(text)}`
+    }else{
+        return `no`
+    }
+   }
+
+   console.log(findArrayIndex(ejemplo, "Ajolote"))
