15784c24695b6011fb350e1bba00e446b5cd6797
lemonlux <sololucia17@gmail.com>
2023-10-02 16:51:27 +0200

    es61

---
+++ JAVASCRIPT/8. JS ES6/ej.js
@@ -0,0 +1,17 @@
+// ---------------- ITERACION 1 -------------
+
+//1.1
+
+const functionAB = (a=10, b=5) =>{
+    return a+b
+}
+
+console.log(functionAB())
+
+//1.2
+
+console.log(functionAB(13,))
+
+//1.3
+
+console.log(functionAB(200,200))
