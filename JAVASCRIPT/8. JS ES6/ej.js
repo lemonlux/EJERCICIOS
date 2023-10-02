aece8550bb7bc1eaac183f0269ec3438a43e8639
lemonlux <sololucia17@gmail.com>
2023-10-02 18:02:37 +0200

    iteracion3

--- JAVASCRIPT/8. JS ES6/ej.js
+++ JAVASCRIPT/8. JS ES6/ej.js
@@ -62,3 +62,49 @@ const[year1, year2, year3] = itv
 console.log(year1)
 
 //--------- ITERACION 3 -----------
+console.log("-------3-------")
+
+//3.1
+
+const pointsList = [32, 54, 21, 64, 75, 43]
+
+const pointsListCopy = [...pointsList]
+
+console.log(pointsListCopy)
+
+//3.2
+
+const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'}
+
+const toyCopy = {...toy}
+
+console.log(toyCopy)
+
+//3.3
+
+const pointsList2 = [32, 54, 21, 64, 75, 43];
+const pointsList3 = [54,87,99,65,32];
+
+const newPointsList =[...pointsList2, ...pointsList3]
+
+console.log(newPointsList)
+
+//3.4
+
+const toy2 = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
+
+const allToys = {...toy2, ...toyUpdate}
+
+console.log(allToys)
+
+//3.5
+
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+
+const colors2 = [...colors]
+colors2.splice(1,1)
+
+console.log(colors2)
+
+
