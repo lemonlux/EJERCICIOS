cbfd35fea55067af3934d24cc355ad6eebd3898b
lemonlux <sololucia17@gmail.com>
2023-10-08 15:34:32 +0200

    mapeo

--- JAVASCRIPT/POKEDEX/primerproyecto/src/components/Gallery/Gallery.js
+++ JAVASCRIPT/POKEDEX/primerproyecto/src/components/Gallery/Gallery.js
@@ -4,18 +4,31 @@ const template = () => `
 
 <section id="galleryContainer"></section>
 `
+const allPokemon = []
 const getData = async() =>{
-    const allPokemon = []
+
     for (let i=1; i<151; i++){
         const data = await fetch (`https://pokeapi.co/api/v2/pokemon/${i}`)
         const jsonData= await data.json()
         allPokemon.push(jsonData)
     }
-    console.log(allPokemon)
 }
+console.log(allPokemon)
 
 
 export const printTemplateGallery = () =>{
     document.querySelector("main").innerHTML=template()
     getData() 
 }
+
+const mappeoPokemon = () =>{
+    const allPokemonMap = allPokemon.map((pokemon)=> ({
+        name: pokemon.name,
+        image: pokemon.sprites.other.dream_world.front_default,
+
+    }))
+    console.log(allPokemonMap)
+}
+
+
+mappeoPokemon()
