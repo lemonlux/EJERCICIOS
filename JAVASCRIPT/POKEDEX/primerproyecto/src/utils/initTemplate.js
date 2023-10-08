795c573abbeab7965a41a05fe0a685870ef8142c
lemonlux <sololucia17@gmail.com>
2023-10-08 12:11:59 +0200

    vite

---
+++ JAVASCRIPT/POKEDEX/primerproyecto/src/utils/initTemplate.js
@@ -0,0 +1,10 @@
+import { printTemplateFooter } from "../components/Footer/Footer"
+
+export const  initTemplate = () =>{
+    const app = document.getElementById("app")
+    const header = document.createElement("header")
+    const main = document.createElement("main")
+    const footer = document.createElement("footer")
+    app.append(header, main, footer)
+    printTemplateFooter()
+}
--- JAVASCRIPT/Primera pagina VITE/primerproyecto/main.js
+++
@@ -1,4 +0,0 @@
-import { initTemplate } from "./src/utils/initTemplate";
-import "./style.css";
-
-initTemplate();
