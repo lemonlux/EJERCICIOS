9df51f0ef87677f17cd0d88f6999c8379370a3bd
lemonlux <sololucia17@gmail.com>
2023-10-13 22:03:08 +0200

    iteraciones

--- JAVASCRIPT/9. JS Asynchronous/1.2/app.js
+++ JAVASCRIPT/9. JS Asynchronous/1.2/app.js
@@ -9,20 +9,38 @@ let dataAPI
 
 const url = "https://api.nationalize.io"
 
+let answer
+
 const loadData = async (name) =>{
 try{
     const response = await fetch(`${url}?name=${name}`)
     const data = await response.json()
     dataAPI = data
-    console.log(data)
+    answer = console.log(data)
 } catch (error){
     console.log(error)
 }
 }
 
+const mapeoFunction = () =>{
+    const mapeoAnswer = dataAPI.map (item =>{
+        count:   item.count
+   })
+}
+
+// const div = document.createElement('div')
+
+
+
 button.addEventListener("click", e =>{
   const input = document.querySelector("input").value
   loadData(input)
 
-
+// console.log(loadData(input))
 })
+
+loadData()
+
+document.querySelector('body').append(div)
+
+
