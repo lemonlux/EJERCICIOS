ad0e986160f3898ff9c58a3a0ece5542df45d58d
lemonlux <sololucia17@gmail.com>
2023-10-29 13:05:27 +0100

    controllers

--- NODE/Servidor WEB/9. Proyecto/src/api/controllers/User.controllers.js
+++ NODE/Servidor WEB/9. Proyecto/src/api/controllers/User.controllers.js
@@ -4,10 +4,47 @@ const randomCode = require('../../utils/randomCode')
 const { deleteImgCloudinary } = require('../../middleware/files.middleware')
 const nodemailer = require('nodemailer')
 
+//!----- post
+
+
 const userRegister = async(req,res,next) =>{
+let catchImg = req.file?.path
+
+try {
+await User.syncIndexes() //como es un post hay que sincronizar los indexes
+let confirmationCode = randomCode(100000, 999999)
+const { email, userName } = req.body 
+
+const userExists = await User.findOne(  //ahora encuentra el username con estos parametros
+    { email:req.body.email },
+    { userName: req.body.userName }
+)
+//*findOne() --- conditions
+//*The conditions are cast to their respective SchemaTypes before the command is sent
+
+if (!userExists){
+
+}else{  //ya existe -- borramos imagen y enviamos error
+
+}
 
 
 
+
+
+
+
+
+    
+} catch (error) {
+    req.file?.path && deleteImgCloudinary(catchImg)   //!--??
+    return (
+    res.status(404).json({
+        error: 'error en el catch general',
+        message: error.message
+    }) && next(error)
+    )
+}
 
 
 
