e7f2340c6761337f5c63c55090e4f93ff446a66b
lemonlux <sololucia17@gmail.com>
2023-10-30 21:58:08 +0100

    redirect

--- NODE/Servidor WEB/9. Proyecto/src/api/controllers/User.controllers.js
+++ NODE/Servidor WEB/9. Proyecto/src/api/controllers/User.controllers.js
@@ -105,13 +105,14 @@ con el set y el get, y hacer una función externa de sendEmail que ubicaremos en
 
 //* va a ser igual que el otro
 
-const stateRegister = async (req,res,next) =>{
+const stateRegister = async (req,res,next) =>{    //!!! ERRROR
   let catchImg= req.file?.path
 
   try {
       await User.syncIndexes()
-      let confirmationCode = randomCode()
-      const { userName, userEmail } = req.body    //del model
+      let confirmationCode = randomCode(100000, 999999)
+      const { userName, userEmail } = req.body  
+      console.log(userEmail, userName)  //del model
 
       const userExists = await User.findOne(
         { userEmail: req.body.userEmail }, //ambas son unique por eso se ponen en objetos separados
@@ -119,19 +120,21 @@ const stateRegister = async (req,res,next) =>{
       )
 
         if(!userExists){
-          const newUser = new User({ ...req.body, confirmationCode })
-        
-        req.file
-        ? (newUser.image = req.file.path)
-        : (newUser.image = 'https://pic.onlinewebfonts.com/svg/img_181369.png')
+          const newUser = new User({ ...req.body, confirmationCode });
+          
+          req.file
+          ? (newUser.image = req.file.path)
+          : (newUser.image = 'https://pic.onlinewebfonts.com/svg/img_181369.png');
 
-        try {   //como vamos a usar otro await - otro trycatch
-            const saveUser = await newUser.save()
-
-          if (saveUser){ //si el usuario se ha guardado enviamos el email
-
+        try {   //como vamos a usar otro await - otro trycatch     //?...-----??? EL RANDOM CODE TIENE PARAMETROS
+          console.log("entrooooo en el trycatch")
+          const userSaved = await newUser.save()
+          
+            console.log(newUser)
+          if (userSaved){ //si el usuario se ha guardado enviamos el email
+            
             sendEmail(userEmail, userName, confirmationCode)
-
+            console.log("entrooooo donde el email")
             /* no tenemos acceso a la librería, la asincronia no es nuestra, por lo que vamos a usar
             un timeout */
 
@@ -139,20 +142,18 @@ const stateRegister = async (req,res,next) =>{
               if (getSendEmail()) {   //si es true (devuelve un boolean)
                 setSendEmail(false)  //lo reseteamos a false por si hay que volver a mandarlo
               res.status(200).json({
-                user: saveUser,
+                user: userSaved,
                 confirmationCode
               })
               } else {
                 setSendEmail(false)
                 return res.status(404).json({
-                  user: saveUser,
+                  user: userSaved,
                   confirmationCode: 'error, resend code'   //para el front end
                 })
               }
 
-
-
-            }, 1400);
+            }, 2000);
 
           } 
 
@@ -184,9 +185,71 @@ const stateRegister = async (req,res,next) =>{
 
 }
 
+//!------------------ register con redirect ---------------------
+//* es igual que anteriormente pero cuando comprobemos que el usuario este guardado redirigimos a una pagina que nos envie el codigo
+
+const redirectRegister = async (req,res,next)=>{
+  let catchImg = req.file?.path
+
+  try {
+    await User.syncIndexes() //sincronizamos indexes
+    let confirmationCode = randomCode(100000, 999999) //creamos el codigo de confirmacion
+    const userExist = await User.findOne(    //buscamos el usuario
+      { userEmail: req.body.userEmail },
+      { userName: req.body.userName }
+    )
+      if (!userExist){   //si el usuario no existe hay que hacer uno nuevo
+        const newUser = new User({ ...req.body, confirmationCode}) 
+
+        req.file ? (newUser.image = req.file.path) : (newUser.image = 'https://pic.onlinewebfonts.com/svg/img_181369.png')
+
+        //para guardar tenemos q hacer un await --- trycatch
+        try {
+          const userSaved = await newUser.save()
+
+          if (userSaved){  //!--- aqui viene lo diferente del redirect
+             
+            return res.
+
+
+          }
+
+
+        } catch (error) {
+          rreq.file && deleteImgCloudinary(catchImg)
+          return res.status(404).json({
+            error: 'error en el catch del save',
+            message: error.message
+          })&& next(error)
+        }
+
+
+
+      }else{ //si el user ya existe hay que borrar la imagen del cloudinary y decir q ya existe
+        req.file && deleteImgCloudinary(catchImg)
+        return res.status(409).json("this user already exists")
+      }
+
+
+
+    
+  } catch (error) { //da feedback al usuario y a nosotros
+    req.file && deleteImgCloudinary(catchImg)
+    return res.status(404).json({
+      error: 'error en el catch general',
+      message: error.message
+    })&& next(error)
+  }
 
 
 
 
 
-module.exports = { userRegister };
+
+
+}
+
+
+
+
+module.exports = { userRegister, stateRegister, redirectRegister };
--- NODE/Servidor WEB/9. Proyecto/src/api/routes/User.routes.js
+++ NODE/Servidor WEB/9. Proyecto/src/api/routes/User.routes.js
@@ -1,8 +1,14 @@
-const { userRegister } = require('../controllers/User.controllers');
+const { userRegister, stateRegister, redirectRegister } = require('../controllers/User.controllers');
 const { upload } = require('../../middleware/files.middleware');
 
 const UserRoutes = require('express').Router();
 
 UserRoutes.post('/registerLong', upload.single('image'), userRegister);
+UserRoutes.post('/registerState', upload.single('image'), stateRegister)
+UserRoutes.post('/redirectRegister', upload.single('image'), redirectRegister)
+
+
+
+
 
 module.exports = UserRoutes;
