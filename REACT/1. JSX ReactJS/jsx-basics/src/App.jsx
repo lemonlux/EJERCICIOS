e445c47e8e7ca32ff7164389ce4c0ca5b96bb15b
lemonlux <sololucia17@gmail.com>
2023-11-13 21:03:28 +0100

    react

--- REACT/1. JSX ReactJS/jsx-basics/src/App.jsx
+++ REACT/1. JSX ReactJS/jsx-basics/src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+
 import './App.css'
 
 const  App = ()=> {
@@ -9,7 +8,7 @@ const  App = ()=> {
   return (
     <>
     {}
-    
+
     </>
   )
 }
---
+++ REACT/1. JSX ReactJS/jsx-basics/src/components/H1/H1.jsx
@@ -0,0 +1,6 @@
+export const H1 = () =>{
+
+
+
+    
+}
