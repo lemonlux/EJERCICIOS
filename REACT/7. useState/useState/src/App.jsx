48e854ad91751b8b900fa2e9a251eafe8543643f
lemonlux <sololucia17@gmail.com>
2023-11-15 21:34:02 +0100

    useState

--- REACT/7. useState/useState/src/App.jsx
+++ REACT/7. useState/useState/src/App.jsx
@@ -3,17 +3,19 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [value, setValue] = useState(() =>{
-    return {
-      search: "",
-      urgency: "",
-    }
+  const [state, setState] = useState(() =>{
+    // return {
+    //   search: "",
+    //   urgency: "",
+    // }
   })
+
+
 
   return (
     <>
-      <h2>{value}</h2>
-      <input  type='text' value={value} onChange={(e)=> setValue(e.target.value)}/>
+      <h2>{state}</h2>
+      <input  type='text' value={state} onChange={(e)=> setState(e.target.value)}/>
       
     </>
   )
---
+++ REACT/8. Proyecto useState/myCV/src/CV/CV.js
@@ -0,0 +1,76 @@
+export const CV = {
+    person: {
+      name: "Lucía",
+      surname: "Martínez Cerón",
+      city: "Murcia, España",
+      email: "sololucia17@gmail.com",
+      birthDate: "17/02/1996",
+      image: "",
+      gitHub: "https://github.com/lemonlux/",
+      aboutMe: [
+        {
+          info: "🤖 My armor, it was never a distraction or a hobby, it was a cocoon. I am Iron Man.",
+        },
+        {
+          info: "🔩 CEO of Stark Industries.",
+        },
+        {
+          info: "🕶 Genius, billionaire, playboy, philanthropist.",
+        },
+        {
+          info: "🦾 I do have a responsibility to keep my inventions from evil hands – but I have a greater responsibility to oppose that evil any way I can.",
+        },
+      ],
+    },
+    education: [
+      {
+        name: "Bootcamp Full Stack Developer",
+        date: "sept 2023 - dic 2023",
+        where: "NEOLAND",
+        description:
+          ""
+      },
+      {
+        name: "Máster en Ciencia e Industria Cosmética",
+        date: "feb 2019 - jun 2020",
+        where: "Colegio Químicos de Murcia",
+        description:
+          ""
+      },
+      {
+        name: "Grado en Química",
+        date: "2014 - 2019",
+        where: "Universidad de Murcia",
+        description:
+        "Alumna interna del departamento de Química Orgánica, desarrollando un proyecto de química computacional sobre el que más tarde basé mi TFG"
+      },
+    ],
+    experience: [
+      {
+        name: "Analista de laboratorio cromatográfico",
+        date: "nov 2019 - nov 2023",
+        where: "Iberchem",
+        description:
+          "Mantenimiento de equipos de laboratorio, conocimientos sobre SAP, AMDIS, Agilent Data Analysis a nivel de usuario",
+      }
+    ],
+    languages: {
+      language: "Inglés",
+      level: "C1",
+
+    },
+    hardSkills: [
+      "HTML5",
+      "CSS3",
+      "Javascript",
+      "React",
+      "Node js",
+    ],
+    softSkills: [
+        "Creatividad",
+        "Trabajo en equipo",
+        "Organización",
+        "Resolución de problemas",
+    ]
+  
+  };
