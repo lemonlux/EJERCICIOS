4f5c1d5235d143245c28fca29338ec19118cf303
lemonlux <sololucia17@gmail.com>
2023-11-20 17:10:26 +0100

    fixed error

--- REACT/5. Mini Proyecto/miniproyecto/src/App.jsx
+++ REACT/5. Mini Proyecto/miniproyecto/src/App.jsx
@@ -1,38 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
-import { useEffect } from 'react';
 
-const charactersMock = [
-  {
-    id: 1,
-    name: "Rick Sanchez",
-    status: "Alive",
-  },
-  {
-    id: 2,
-    name: "Morty Smith",
-    status: "Alive",
-  },
-];
 
 
 const App = () => {
-
-  // const [ characters, setCharacters ] = useState(charactersMock)
-  // console.log(characters)
-  const [ characters, setCharacters ] = useState[()]
-
-  // useEffect(()=>{
-   
-  // }, [])
-
-
-  //!---------- hace falta el fetch 
   
+  const [ characters, setCharacters ] = useState([])
+
+  const dataFunction = (async () =>{
+    console.log('hola')
+    let data = await fetch(`https://rickandmortyapi.com/api/character/`).then((res)=> res.json())
+    setCharacters(data.results)
+    
+  })
+
+  useEffect(() =>{
+    dataFunction()
+  }, [])
+
 
   return (
     <>
-     {/* <div>
+     <div>
       {characters.map((item)=>(
         <div key={item.id}>
           <h2>{item.name}</h2>
@@ -40,7 +29,7 @@ const App = () => {
         </div>
        
       ))}
-     </div> */}
+     </div>
     </>
   )
 }
